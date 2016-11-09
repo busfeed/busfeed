0f6468fb0fd3c2add10fb68d995284d84402477a
Michael Du <m2du@ucsd.edu>
2016-11-09 08:32:50 -0800

    labels added to graph axes

--- public/js/d3details.js
+++ public/js/d3details.js
@@ -4,7 +4,8 @@ function drawChart(day) {
       $('#chart').empty();
 
       var width = $(window).width();
-      var height = 210;
+      var height = 220;
+      var margins = {"top": 5, "bot": 35, "left": 38, "right": 0};
 
 
       var curr_hour = (parseInt(new Date().getHours()) - 7)*2;
@@ -13,11 +14,9 @@ function drawChart(day) {
 
       if (curr_hour - 5 < lower) {
          higher = lower + 9;
-         console.log("lower");
       }
       else if (curr_hour + 4 >= higher) {
          lower = higher - 9;
-         console.log("higher");
       }
       else {
          lower = curr_hour - 5;
@@ -32,17 +31,23 @@ function drawChart(day) {
       .attr("viewbox", "0 0 "+width+" "+height)
       .append("g");
 
-      var xScale = d3.scaleOrdinal([20, 56, 92, 128, 164, 200, 236, 272, 308]).domain(json.domain.slice(lower,higher));
-      var yScale = d3.scaleLinear().rangeRound([height-30, 0]).domain([0, d3.max(json.range.slice(lower,higher))]);
+      //308 max
+      var xTickLocs = [];
+      for (var i=0; i < 9; i++) {
+         xTickLocs.push(margins.left+34*i);
+      }
+
+      var xScale = d3.scaleOrdinal(xTickLocs).domain(json.domain.slice(lower,higher));
+      var yScale = d3.scaleLinear().rangeRound([height-margins.bot, margins.top]).domain([0, d3.max(json.range.slice(lower,higher))]);
       var xAxis = d3.axisBottom(xScale);
       var yAxis = d3.axisLeft(yScale).tickFormat(function(d) { if (Math.floor(d) != d) { return; } return d;});
 
       svg.append("g")
-      .attr("transform", "translate(0," + (height-25) + ")")
+      .attr("transform", "translate(0," + (height-margins.bot) + ")")
       .call(xAxis);
 
       svg.append("g")
-      .attr("transform", "translate(20,5)")
+      .attr("transform", "translate("+margins.left+",0)")
       .call(yAxis);
 
       var line = d3.line()
@@ -63,5 +68,33 @@ function drawChart(day) {
       .attr('stroke', 'red')
       .attr('stroke-width', 2)
       .attr('fill', 'none');
+
+      svg.selectAll("dot")
+      .data(data)
+      .enter().append("circle")
+      .attr("r", 3.5)
+      .attr("cx", function(d) { return xScale(d.x); })
+      .attr("cy", function(d) { return yScale(d.y); })
+      .style("fill", "DarkRed");
+
+
+      //Create X axis label
+      svg.append("text")
+      .attr("x", (width-margins.left) / 2 )
+      .attr("y", height-3)
+      .style("text-anchor", "middle")
+      .style("font-weight", "bold")
+      .text("Time of Day");
+
+      //Create Y axis label
+      svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0)
+      .attr("x", 0 - margins.top - ((height-margins.top-margins.bot) / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .style("font-weight", "bold")
+      .text("Crowding Intensity");
    });
+
 }
