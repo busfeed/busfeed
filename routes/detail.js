0129fd0647fe93460fb218e5c453dac2f8a75a6f
Michael Du <m2du@ucsd.edu>
2016-11-10 20:42:40 -0800

    linked geo.json with google map functions

--- routes/detail.js
+++ routes/detail.js
@@ -1,16 +1,19 @@
 var dotenv = require('dotenv');
 var allstops = require('../data/allstops');
 var data = require('../routes/data');
+var geos = require('../data/geo');
 
 dotenv.load();
 
 exports.view = function(req, res) {
+   var stopId = parseInt(req.params.stopId);
+   console.log(geos.locations[stopId].id);
    res.render('detail', {
       "stopId": req.params['stopId'],
-      "stopName": allstops.stops[parseInt(req.params['stopId'])],
+      "stopName": allstops.stops[stopId],
       "goBack": req.header('Referer'),
       "tracked": data.feedList.feed.indexOf(req.params.stopId) > -1,
-      "loc": {"lat": 32.87715, "long": -117.23571},
+      "loc": geos.locations[stopId].geo,
       "mapsKey": process.env.GOOGLE_API_KEY
    });
 };
