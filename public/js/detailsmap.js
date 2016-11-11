c45807f7b795ea846c7b91429614040efb6990c9
Michael Du <m2du@ucsd.edu>
2016-11-10 17:11:30 -0800

    removed unused code in detailsmap.js

--- public/js/detailsmap.js
+++ public/js/detailsmap.js
@@ -59,17 +59,9 @@ function initMap() {
 
          map.fitBounds(bounds);
       }, function() {
-         handleLocationError(true, infoWindow, map.getCenter());
+         alert("Geolocation service failed");
       });
    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+      alert("Geolocation not enabled");
    }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-   infoWindow.setPosition(pos);
-   infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
-      'Error: Your browser doesn\'t support geolocation.');
 }
